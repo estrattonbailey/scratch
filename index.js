09a7d75e0409e114f7c468d8bf0b39472907433e
estrattonbailey <ericstrattonbailey@gmail.com>
2020-01-11 12:03:05 -0600

    fixes #2, fixes #3

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
 #! /usr/bin/env node
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const path = require('path')
-const mri = require('mri')
-const c = require('ansi-colors')
-const { parse } = require('@babel/parser')
-const generate = require('@babel/generator').default
+const fs = require("fs");
+const path = require("path");
+const mri = require("mri");
+const c = require("ansi-colors");
+const { parse } = require("@babel/parser");
+const generate = require("@babel/generator").default;
 
-const pkg = require('./package.json')
+const pkg = require("./package.json");
 
-const cwd = process.cwd()
-const { _ } = mri(process.argv.slice(2))
-const file = _[0] ? path.resolve(cwd, _[0]) : path.resolve(cwd, `./scratch_${Date.now()}.js`)
-const banner = `scratch v${pkg.version}`
+const cwd = process.cwd();
+const { _ } = mri(process.argv.slice(2));
+const file = _[0]
+  ? path.resolve(cwd, _[0])
+  : path.resolve(cwd, `./scratch_${Date.now()}.js`);
+const banner = `scratch v${pkg.version}`;
 
-console.clear()
-console.log(c.green(banner))
+console.clear();
+console.log(c.green(banner));
 
 try {
-  require.resolve(file)
+  require.resolve(file);
 } catch (e) {
-  fs.closeSync(fs.openSync(file, 'w'))
+  fs.closeSync(fs.openSync(file, "w"));
 }
 
-function run () {
+const reqRegEx = /require\('(.+)'\)/;
+
+function run() {
   try {
-    const code = fs.readFileSync(file, 'utf8')
-    const ast = parse(code)
+    const code = fs.readFileSync(file, "utf8");
 
-    const transpiled = generate(ast, {}, code).code
+    const files = code
+      .match(new RegExp(reqRegEx, 'gm'))
+      .map(c => c.match(reqRegEx)[1])
+      .filter(p => Boolean(p));
 
-    console.clear()
-    console.log(c.green(banner) + '\n')
-    const res = eval(transpiled)
-    if (res) console.log('\n> ' + res)
+    files.forEach(file => {
+      delete require.cache[require.resolve(cwd, file)]
+    });
+
+    const ast = parse(code.replace(/\.\//g, cwd + "/"));
+
+    const transpiled = generate(ast, {}, code).code;
+
+    console.clear();
+    console.log(c.green(banner) + "\n");
+    const res = eval(transpiled);
+    if (res) console.log("\n> " + res);
   } catch (e) {
-    console.clear()
-    console.log(c.red(banner) + '\n')
-    console.log(e)
+    console.clear();
+    console.log(c.red(banner) + "\n");
+    console.log(e);
   }
 }
 
-run()
+run();
 
-require('chokidar').watch(file).on('change', run)
+require("chokidar")
+  .watch(file)
+  .on("change", run);
