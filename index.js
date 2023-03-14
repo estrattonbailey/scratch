e6b43d06431bcf3e18fa57c43e08b099ce40e3ad
Eric Bailey <git@esb.lol>
2023-03-14 12:19:49 -0500

    enhance

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require('esbuild-register/dist/node').register()
 const fs = require("fs");
 const path = require("path");
 const c = require("kleur");
+require('consola').create({
+  level: 5,
+}).wrapConsole()
 
 const pkg = require("./package.json");
 
@@ -13,13 +16,12 @@ const banner = `scratch v${pkg.version}`;
 const cwd = process.cwd();
 const [entry] = process.argv.slice(2)
 
-console.clear();
-console.log(c.green(banner) + "\n");
-
 if (!entry) {
   console.error(`\n${c.yellow(banner)}\n\nUsage: npx ${pkg.name} ${c.green('<file>')}\n`)
   process.exit(1)
 }
+
+fs.mkdirSync(path.join(cwd, ".scratch"), { recursive: true });
 
 const file = path.resolve(cwd, entry)
 
@@ -30,8 +32,22 @@ try {
 }
 
 function run() {
+  console.clear();
+  console.log(c.green(banner) + "\n");
+
   try {
-    require(file)
+    const outfile = path.join(cwd, ".scratch", path.basename(file).split(".").reverse().slice(1).reverse() + ".js");
+    try {
+      delete require.cache[require.resolve(outfile)]
+    } catch (e) {}
+    require('esbuild').buildSync({
+      entryPoints: [file],
+      bundle: false,
+      outfile,
+      sourcemap: true,
+      format: "esm",
+    })
+    require(outfile)
   } catch (e) {
     console.error(e);
   }
